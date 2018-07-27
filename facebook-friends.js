c63fa21069b7a16777a251c31003fe24b70e6d03
David Blackwelder <dblackw2@gmail.com>
2018-07-26 23:01:29 -0400

    updated Facebook Friends programming quiz

---
+++ facebook-friends.js
@@ -0,0 +1,35 @@
+/*
+ * Programming Quiz: Facebook Friends (7-5)
+ * 
+ * Directions:
+Create an object called facebookProfile. The object should have 3 properties:
+
+your name
+the number of friends you have, and
+an array of messages you've posted (as strings)
+The object should also have 4 methods:
+
+postMessage(message) - adds a new message string to the array
+deleteMessage(index) - removes the message corresponding to the index provided
+addFriend() - increases the friend count by 1
+removeFriend() - decreases the friend count by 1
+ */
+
+// your code goes here
+var facebookProfile = {
+    name: "David Blackwelder",
+    numberOfFriends: 340,
+    messagesPosted: ["Hello", "Today is Thursday", "I am glad to be learning to code"],
+    postMessage: function addMessage(message) {
+        facebookProfile.messagesPosted.push(message);
+    },
+    deleteMessage: function removeMessage(index) {
+        facebookProfile.messagesPosted.splice(index, 1);
+    },
+    addFriend: function newFriend() {
+        facebookProfile.numberOfFriends += 1;
+    },
+    removeFriend: function deleteFriend() {
+        facebookProfile.numberOfFriends -= 1;
+    }
+};
